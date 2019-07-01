5cd62a71c40276192c7b261c2fe0a3bf5f6ec3f8
Muhammad Fahad Ameen <mfah@ciklum.com>
2019-07-01 12:28:37 +0500

    added person and personContext class

---
+++ WebApiProject/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace WebApiProject.Models
+{
+    public class Person
+    {
+        [Key]
+        public string Name { get; set; }
+    }
+}
