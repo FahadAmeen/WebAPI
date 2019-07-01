ddbb1a842d5f368d25b70d0ddd71220d7cf5442b
Muhammad Fahad Ameen <mfah@ciklum.com>
2019-07-01 14:49:04 +0500

    person table is created final v1

---
+++ WebApiProject/Controllers/PeopleController.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiProject.Data;
+using WebApiProject.Models;
+
+namespace WebApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly PersonContext _context;
+
+        public PeopleController(PersonContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/People
+        [HttpGet]
+        public IEnumerable<Person> GetPersons()
+        {
+            return _context.Persons;
+        }
+
+        // GET: api/People/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPerson([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var person = await _context.Persons.FindAsync(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+
+        // PUT: api/People/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPerson([FromRoute] string id, [FromBody] Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != person.Name)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(person).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/People
+        [HttpPost]
+        public async Task<IActionResult> PostPerson([FromBody] Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Persons.Add(person);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPerson", new { id = person.Name }, person);
+        }
+
+        // DELETE: api/People/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePerson([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var person = await _context.Persons.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            _context.Persons.Remove(person);
+            await _context.SaveChangesAsync();
+
+            return Ok(person);
+        }
+
+        private bool PersonExists(string id)
+        {
+            return _context.Persons.Any(e => e.Name == id);
+        }
+    }
+}
