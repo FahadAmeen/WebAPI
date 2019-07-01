2fa55ebc714ba5bfeec801a459bfb49d58538aa9
Muhammad Fahad Ameen <mfah@ciklum.com>
2019-07-01 14:19:05 +0500

    person table is created final v1

---
+++ WebApiProject/Data/Migrations/20190701081124_InitialCreate.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApiProject.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Name);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
--- WebApiProject/Data/PersonContext.cs
+++ WebApiProject/Data/PersonContext.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiProject.Models;
 
 namespace WebApiProject.Data
 {
-    public class PersonContext
+    public class PersonContext:DbContext
     {
+        public PersonContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Person> Persons { get; set; }
+
     }
 }
