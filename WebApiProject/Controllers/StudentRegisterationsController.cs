2496c681692b18bd3b619a98a1eeb30ca9f09600
saharejaz2324 <sej@ciklum.com>
2019-07-06 21:18:49 +0500

    ,,,

--- WebApiProject/Controllers/StudentRegisterationsController.cs
+++ WebApiProject/Controllers/StudentRegisterationsController.cs
@@ -26,60 +26,87 @@ namespace WebApiProject.Controllers
 
 
 
-        //        var users = _context.Users.OrderBy(p => EF.Property<object>(p, sortBy));
+//        var users = _context.Users.OrderBy(p => EF.Property<object>(p, sortBy));
 
-        //        var selectUsers = from s in users select s;
+//        var selectUsers = from s in users select s;
 
-        //            if (!String.IsNullOrEmpty(forWord))
-        //            {
-        //                switch (inColumn)
-        //                {
-        //                    case "Id":
-        //                        selectUsers = selectUsers.Where(s => s.Id == Int32.Parse(forWord));
-        //                        break;
+//                    if (!String.IsNullOrEmpty(forWord))
+//                    {
+//                        switch (inColumn)
+//                        {
+//                            case "Id":
+//                                selectUsers = selectUsers.Where(s => s.Id == Int32.Parse(forWord));
+//                                break;
 
-        //                    default:
-        //                        selectUsers = selectUsers.Where(s => EF.Property<string>(s, inColumn).Contains(forWord));
-        //                        break;
-        //                }
-        //}
+//                            default:
+//                                selectUsers = selectUsers.Where(s => EF.Property<string>(s, inColumn).Contains(forWord));
+//                                break;
+//                        }
+//}
 
-        //            if (pageNo > -1)
-        //            {
-        //                return await selectUsers.Skip(pageNo* pageSize).Take(pageSize).ToArrayAsync();
-        //            }
-        //            else
-        //            {
-        //                return await selectUsers.ToArrayAsync();
-        //            }
+//                    if (pageNo > -1)
+//                    {
+//                        return await selectUsers.Skip(pageNo* pageSize).Take(pageSize).ToArrayAsync();
+//                    }
+//                    else
+//                    {
+//                        return await selectUsers.ToArrayAsync();
+//                    }
 
-        //        }
+//                }
 
-        // GET: api/StudentRegisterations/Get
-        [HttpGet("Get")]
-        public async Task<IList<StudentRegisteration>> GetStudentRegisterations(int pageNo = 1, string searchWith = "id", string searchData = "", string sortData = "id", int pageSize = 5)
+        // GET: api/StudentRegisterations
+        [HttpGet]
+        public async Task<IList<StudentRegisteration>> GetStudentRegisterations(int pageNo = 1, string searchWith = "", string searchData = "", string sortData = "", int pageSize = 5)
         {
-            pageNo = pageNo - 1;
-            sortData = sortData.ToLower();
-            searchWith = searchWith.ToLower();
-         
-            var user = _context.StudentRegisterations.OrderBy(p => EF.Property<string>(p, sortData));
 
-            var users = from s in _context.StudentRegisterations select s;
+            var users = _context.Users.OrderBy(p => EF.Property<object>(p, sortData));
+
+            var selectUsers = from s in users select s;
 
             if (!String.IsNullOrEmpty(searchData))
             {
-                if ((searchWith == "name") || (searchWith == "program") || searchWith == "detail" || (searchWith == "filename"))
+                switch (searchWith)
                 {
-                    users = users.Where(s => EF.Property<string>(s, searchWith).Contains(searchData));
+                    case "Id":
+                        selectUsers = selectUsers.Where(s => s.Id == Int32.Parse(searchData));
+                        break;
+
+                    default:
+                        selectUsers = selectUsers.Where(s => EF.Property<string>(s, searchWith).Contains(searchData));
+                        break;
                 }
             }
-            else
-            {
-                users = users.Where(s => s.Id == Int32.Parse(searchData));
-            }
-            return await users.Skip(pageNo * pageSize).Take(pageSize).ToArrayAsync();
+            return await selectUsers.Skip(pageNo * pageSize).Take(pageSize).ToArrayAsync();
         }
+
+
+
+
+
+
+
+        //pageNo = pageNo - 1;
+        //    sortData = sortData.ToLower();
+        //    searchWith = searchWith.ToLower();
+         
+        //    var user = _context.StudentRegisterations.OrderBy(p => EF.Property<string>(p, sortData));
+
+        //    var users = from s in _context.StudentRegisterations select s;
+
+        //    if (!String.IsNullOrEmpty(searchData))
+        //    {
+        //        //if ((searchWith == "name") || (searchWith == "program") || searchWith == "detail" || (searchWith == "filename"))
+        //        //{
+        //            users = users.Where(s => EF.Property<string>(s, searchWith).Contains(searchData));
+        //        //}
+        //    }
+        //    else
+        //    {
+        //        users = users.Where(s => s.Id == Int32.Parse(searchData));
+        //    }
+        //    return await users.Skip(pageNo * pageSize).Take(pageSize).ToArrayAsync();
+        //}
         //    user = _context.StudentRegisterations.OrderBy(StudentRegisteration => StudentRegisteration.Id);
 
         //    switch (sortData)
